20579b29c02528d0be8d552425723ea7c7d2d474
Julia <julso@LAPTOP-J5CQF40T.(none)>
2022-11-29 17:11:10 +0300

    Seminar5 1 added

---
+++ Seminar5_1/Program.cs
@@ -0,0 +1,35 @@
+﻿int size = 12;
+int [] numbers = new int [size];
+FillArrayRandomNumbers(numbers,-9,9);
+WriteArray(numbers);
+
+int sumPositive = 0;
+int sumNegative = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 0)
+        sumPositive += numbers[i];
+    else
+        sumNegative += numbers[i];
+}
+
+Console.WriteLine($"Сумма положительных равна {sumPositive}, Сумма отрицательных равна {sumNegative}");
+
+
+void FillArrayRandomNumbers(int[] array, int min, int max)
+{
+   for(int i = 0; i < array.Length; i++) 
+   {
+    array[i] = new Random().Next(min, max + 1);
+   }
+}
+
+void WriteArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+    Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
