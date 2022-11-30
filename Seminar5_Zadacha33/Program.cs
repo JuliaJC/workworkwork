ec7a9db59d0fc11924d71d4cbfb17157e44681a8
Julia <julso@LAPTOP-J5CQF40T.(none)>
2022-11-30 10:38:47 +0300

    Seminar5 Zadacha33 added

--- Seminar5_Zadacha33/Program.cs
+++ Seminar5_Zadacha33/Program.cs
@@ -3,7 +3,56 @@
 4; массив [6, 7, 19, 345, 3] -> нет
 3; массив [6, 7, 19, 345, 3] -> да*/
 
-Console.Write("Введите размер массива: ");
+int size = ReadInt("Введите размер массива: ");
+int[] numbers = new int[size];
+
+FillArrayRandomNumbers(numbers, 1, 10);
+WriteArray(numbers);
+
+int searchNumber = ReadInt("Введите искомое число: ");
+bool exist = false;
+
+for(int i = 0; i < numbers.Length; i++)
+{
+    if (searchNumber == numbers[i])
+    {
+        exist = true;
+        break;
+    }
+}
+if(exist)
+    Console.WriteLine("Да");
+else
+    Console.WriteLine("Нет");
+
+
+void FillArrayRandomNumbers(int[] array, int min, int max)
+{
+for(int i = 0; i < array.Length; i++)
+{
+array[i] = new Random().Next(min, max + 1);
+}
+}
+
+void WriteArray(int[] array)
+{
+for(int i = 0; i < array.Length; i++)
+{
+Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
+
+
+
+
+/*Console.Write("Введите размер массива: ");
 int size = Convert.ToInt32(Console.ReadLine());
 
 int [] numbers = new int [size];
@@ -43,4 +92,4 @@ void WriteArray(int[] array)
     Console.Write(array[i] + " ");
     }
     Console.WriteLine();
-}
+}*/
