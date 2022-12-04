1cf8a855b5e24e3f2f1d9b5104b337d662cce17b
Julia <julso@LAPTOP-J5CQF40T.(none)>
2022-12-04 15:35:32 +0300

    Seminar6_Zadacha42 added

---
+++ Seminar6_Zadacha42/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Задача 42: Напишите программу, которая будет преобразовывать 
+десятичное число в двоичное.
+45 -> 101101
+3 -> 11
+2 -> 10*/
+
+int number = ReadInt("Введите десятичное число: ");
+
+int temp = number;
+string bynary = "";
+
+while(temp != 0)
+{
+    bynary += Convert.ToString(temp%2);
+    temp /= 2;
+}
+string result = "";
+for(int i = bynary.Length-1; i >=0; i--) 
+{
+    result += bynary [i];
+}
+
+
+Console.WriteLine(bynary);
+Console.WriteLine(result);
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
