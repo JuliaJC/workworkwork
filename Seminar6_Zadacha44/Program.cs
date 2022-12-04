00c9e16bd271001624213982f7e9d70662949a1f
Julia <julso@LAPTOP-J5CQF40T.(none)>
2022-12-04 16:47:32 +0300

    Seminar6_Zadacha44 added

---
+++ Seminar6_Zadacha44/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. 
+Первые два числа Фибоначчи: 0 и 1.
+Если N = 5 -> 0 1 1 2 3
+Если N = 3 -> 0 1 1
+Если N = 7 -> 0 1 1 2 3 5 8*/
+
+int number;
+
+while(true)
+{
+number = ReadInt("Введите число: ");
+if(number>0) break;
+else
+Console.WriteLine("Введите число больше 0.");
+}
+int[] fibonacci = new int[number];
+fibonacci[0]=0;
+fibonacci[1]=1;
+
+for(int i = 2; i<number; i++)
+{
+    fibonacci[i] = fibonacci[i-1]+fibonacci[i-2];
+}
+
+Console.WriteLine($"Числа Фибоначчи до {number}: {String.Join(", ", fibonacci)}");
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
